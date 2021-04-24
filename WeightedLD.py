33adf9a2b78fec806820085d97cf2ac67579d862
Oscar Charles <oscarjamescharles@gmail.com>
2021-04-24 13:31:31 +0100

    python tests and varsite fixes

--- WeightedLD.py
+++ WeightedLD.py
@@ -1,17 +1,10 @@
 # Oscar Charles 210404
-# Steven Henikoff and Jorja G. Henikoff (1994) "Position-based Sequence Weights" 
-# using matt hahn molpopgen book
-# This code calculates sequence weights using the Henikoff formula from a multiple sequuence alignment
+# This code calculates sequence weights using the Henikoff formula from a multiple sequence alignment
 # usage python WeightedLD.py alignment.fasta
-
-#---------- todo
-# sequence weighting - treat 4 as a not ok character
-# LD - why are some values being returned infinte?
 
 import logging
 import argparse
 from pathlib import Path
-
 from Bio import AlignIO
 import numpy as np
 
@@ -22,10 +15,17 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 
+
 def read_fasta(filename: Path) -> np.ndarray:
+    """
+    Converts a fasta DNA alignment file to a hashed integer matrix
+    """
     raw_alignment = AlignIO.read(filename, "fasta")
 
-    alignment_chars = np.zeros((len(raw_alignment), raw_alignment.get_alignment_length()), dtype='<U1')
+    alignment_chars = np.zeros(
+        (len(raw_alignment), raw_alignment.get_alignment_length()), dtype='<U1')
+
+    # forces lowercase IUPAC characters
     for (iSeq, seq) in enumerate(raw_alignment):
         alignment_chars[iSeq, :] = list(str(seq.seq).lower())
 
@@ -35,12 +35,13 @@ def read_fasta(filename: Path) -> np.ndarray:
     alignment[alignment_chars == 'g'] = 2
     alignment[alignment_chars == 't'] = 3
     alignment[alignment_chars == '-'] = 4
-    
+
     return alignment
+
 
 def compute_variable_sites(alignment: np.ndarray, min_acgt: float, min_variability: float) -> np.ndarray:
     """
-    Which sites have both sufficient data and a significant enough quantity of a minor symbol
+    returns which sites have both sufficient data and a significant enough quantity of a minor symbol
 
     Args:
         alignment: 2D numpy array where:
@@ -50,43 +51,56 @@ def compute_variable_sites(alignment: np.ndarray, min_acgt: float, min_variabili
               represented by (0, 1, 2, 3, 4, 5)
         min_acgt: The minimum fraction of sequences which must have A/C/G/T
             symbols at a given site
-        min_variability: The minimum fraction of sequences which have the minor
+        min_variability: The minimum fraction of sequences which have the minor 
             symbol at a given site, ignoring sequences that have missing or
             ambiguous symbols.
 
     Returns:
-        A 1D boolean array of length n_sites, which is True for each site that
-        meets the criteria
+        2 member tuple of 1D boolean arrays of length n_sites, which is True for each site that
+        meets the criteria.
+        1 relevant for Henikoff calculations
+        2 relevant for LD calculations
     """
-    
+
     # For each site, the fraction of sequences which have a concrete symbol at that position
     concrete_fraction = (alignment < 4).sum(axis=0) / alignment.shape[0]
-    
+
     # Whether a given site has enough data to be considered for further processing
     sufficient_data = concrete_fraction > min_acgt
 
     # For each site, how many sequences have a given symbol at that site
-    acgt_counts = np.array([(alignment == x).sum(axis=0) for x in (0, 1, 2, 3)])
+    acgt_counts = np.array([(alignment == x).sum(axis=0)
+                           for x in (0, 1, 2, 3, 4)])
 
+    # Whether each site has multiple non-ambiguous symbols - deprecated
+    # multiple_non_ambiguous = np.sum(acgt_counts > 0, axis=0) > 1
     major_counts = acgt_counts.max(axis=0)
     minor_counts = acgt_counts.sum(axis=0) - major_counts
 
+    # sites with any variation
     f = minor_counts > 0
     minor_fraction = np.zeros(alignment.shape[1])
     minor_fraction[f] = minor_counts[f] / (major_counts[f] + minor_counts[f])
-    
+
     # Does the minor symbol occur at a high enough frequency
     has_min_variability = minor_fraction >= min_variability
-    
-    # Whether each site has multiple non-ambiguous symbols
-    multiple_non_ambiguous = np.sum(acgt_counts > 0, axis=0) > 1
-    
-    return multiple_non_ambiguous & sufficient_data & has_min_variability
-    
+
+    # consider a small set of sequences from a recombining population, there could be multiple SNP clues occuring in a small fraction of sequences that together help define a cluster by pairwise distance
+    # these are the only filters we want to apply for weighting - as we want to allow for  very minor population SNP's
+    # any variability
+    return_hk_varsites = sufficient_data & f
+    # has enough variability to return useful LD data
+    return_ld_varsites = sufficient_data & has_min_variability
+
+    return return_hk_varsites, return_ld_varsites
+
 
 def henikoff_weighting(alignment: np.ndarray) -> np.ndarray:
     """
     The Henikoff weighting for each sequence in the given alignment array.
+
+    ref:
+        Steven Henikoff and Jorja G. Henikoff (1994) "Position-based Sequence Weights"
 
     Args:
         alignment: 2D numpy array where:
@@ -95,46 +109,48 @@ def henikoff_weighting(alignment: np.ndarray) -> np.ndarray:
             - Each element is an integer where (A, C, G, T, -, ambiguous) is
               represented by (0, 1, 2, 3, 4, 5)
             - Only sites of interest are included
-    
+
     Returns:
         A 1D numpy array of length n_seqs, containing the Henikoff weight of each sequence.
     """
-    
+
     n_sites = alignment.shape[1]
     n_seqs = alignment.shape[0]
-    
+
     # Mask for non-ambiguous bases (False where the base is ambiguous)
     ok_base = (alignment != 5)
-    
+
     # For each site, the count of sequences that have a given symbol at that site
     # Eg count_base[1, 1234] contains the count of sequences that have symbol 1 at site 1234
     count_base = np.zeros((6, n_sites))
     for base in range(6):
         count_base[base, :] = (alignment == base).sum(axis=0)
-        
-    # For each site, the count of sequences with a concrete symbol at that site
+
+    # For each site, the count of sequences with an informative nucleotide at that site
     t_seqs = np.sum(count_base[:5, :], axis=0)
-    
+
     site_contribution = np.zeros(alignment.shape)
-    site_contribution[ok_base] = 1 / (t_seqs * count_base[alignment, np.arange(n_sites)])[ok_base]
-    
+    site_contribution[ok_base] = 1 / \
+        (t_seqs * count_base[alignment, np.arange(n_sites)])[ok_base]
+
     # For each ambiguous base, fill in the average contribution across all
     # sequences with concrete bases for that site
     site_contribution[~ok_base] = 0
     site_average_weight = site_contribution.sum(axis=0) / t_seqs
-    site_contribution[~ok_base] = np.broadcast_to(site_average_weight, site_contribution.shape)[~ok_base]
-    
+    site_contribution[~ok_base] = np.broadcast_to(
+        site_average_weight, site_contribution.shape)[~ok_base]
+
     # For each sequence, the sum of contributions from each site
     weights = site_contribution.sum(axis=1)
 
     # Normalize such that the largest weight is exactly 1
     return weights / weights.max()
 
-    
+
 def ld(alignment, weights, site_map):
     """
     Generate LD metrics; D, D', and R2
-    
+
     Args:
         alignment: 2D numpy array where:
             - The first axis represents sequences
@@ -144,14 +160,14 @@ def ld(alignment, weights, site_map):
             - Only sites of interest should be included
         weights: 1D numpy array of length n_seqs, containing the relative weight of each sequence
         site_map: Some object that can map site indices in the alignment argument to meaningful site indices
-    
+
     Returns:
         Nothing, just prints the results to stdout in a tab-separated format
     """
-    
+
     n_seqs = alignment.shape[0]
     n_sites = alignment.shape[1]
-    
+
     # stdout headers
     print("posa\tposb\tD\tD'\tR2")
     for first_site in range(n_sites - 1):
@@ -165,7 +181,7 @@ def ld(alignment, weights, site_map):
             target_sites = target_sites[good_sequences, :]
             target_weights = weights[good_sequences]
             target_seqs = target_sites.shape[0]
-            
+
             # For each of the sequence, the "major allele" is the symbol that occurs most frequently
 
             # Whether the given sequence is equal to the major symbol at the given site
@@ -173,21 +189,24 @@ def ld(alignment, weights, site_map):
 
             skip_site = False
             for site in (0, 1):
-                unique_elements, counts = np.unique(target_sites[:, site], return_counts=True)
+                unique_elements, counts = np.unique(
+                    target_sites[:, site], return_counts=True)
                 if len(unique_elements) == 1:
                     # After removing the bad sequences, one or both of the
                     # sites may no longer be variable. Stop calculations here
                     # if that is the case.
                     skip_site = True
                 major_symbol = unique_elements[counts.argmax()]
-                target_sites_major[target_sites[:, site] == major_symbol, site] = True
+                target_sites_major[target_sites[:, site]
+                                   == major_symbol, site] = True
             if skip_site:
                 continue
-            
+
             total_weight = target_weights.sum()
-            PA, PB = np.ma.masked_array(target_weights.reshape(-1, 1).repeat(2, axis=1), ~target_sites_major).sum(axis=0) / total_weight
-            Pa, Pb = np.ma.masked_array(target_weights.reshape(-1, 1).repeat(2, axis=1), target_sites_major).sum(axis=0) / total_weight
-            
+            PA, PB = np.ma.masked_array(target_weights.reshape(-1, 1).repeat(
+                2, axis=1), ~target_sites_major).sum(axis=0) / total_weight
+            Pa, Pb = np.ma.masked_array(target_weights.reshape(-1, 1).repeat(
+                2, axis=1), target_sites_major).sum(axis=0) / total_weight
 
             # ----- predicted allele freqs if 0 LD
             PAB = PA * PB
@@ -195,77 +214,89 @@ def ld(alignment, weights, site_map):
             PaB = Pa * PB
             Pab = Pa * Pb
 
-            
             # ----- observed allelle frequencies
             ld_obs = np.zeros(4)
-            ld_obs[0] = target_weights[~target_sites_major[:, 0] & ~target_sites_major[:, 1]].sum()
-            ld_obs[3] = target_weights[target_sites_major[:, 0] & target_sites_major[:, 1]].sum()
-            ld_obs[1] = target_weights[~target_sites_major[:, 0] & target_sites_major[:, 1]].sum()
-            ld_obs[2] = target_weights[target_sites_major[:, 0] & ~target_sites_major[:, 1]].sum()
+            ld_obs[0] = target_weights[~target_sites_major[:, 0]
+                                       & ~target_sites_major[:, 1]].sum()
+            ld_obs[3] = target_weights[target_sites_major[:, 0]
+                                       & target_sites_major[:, 1]].sum()
+            ld_obs[1] = target_weights[~target_sites_major[:, 0]
+                                       & target_sites_major[:, 1]].sum()
+            ld_obs[2] = target_weights[target_sites_major[:, 0]
+                                       & ~target_sites_major[:, 1]].sum()
             ld_obs = ld_obs / total_weight
 
             # ----- Caclulate D [Linkage Disequilibrium]
             # the vector is now as in the hahn molpopgen book and can be used to generate D values
             # ld_obs is pAB, p
             tD = np.zeros(4)
-            tD[0] = PAB - ld_obs[3] # MajMaj
-            tD[1] = Pab - ld_obs[0] # minmin  
-            tD[2] = -1 * (PAb - ld_obs[2]) # Majmin
-            tD[3] = -1 * (PaB - ld_obs[1]) # minMaj
-            D = (tD[0] + tD[1] + tD[2] + tD[3]) / 4     #they should be the same anyhow
+            tD[0] = PAB - ld_obs[3]  # MajMaj
+            tD[1] = Pab - ld_obs[0]  # minmin
+            tD[2] = -1 * (PAb - ld_obs[2])  # Majmin
+            tD[3] = -1 * (PaB - ld_obs[1])  # minMaj
+            D = (tD[0] + tD[1] + tD[2] + tD[3]) / \
+                4  # they should be the same anyhow
 
             # normalised D = D'
             if D < 0:
-                denominator = max([-ld_obs[0],-ld_obs[3]])
+                denominator = max([-ld_obs[0], -ld_obs[3]])
                 if denominator == 0:
-                    denominator = min([-ld_obs[0],-ld_obs[3]])
+                    denominator = min([-ld_obs[0], -ld_obs[3]])
             else:
-                denominator = min([ld_obs[1],ld_obs[2]])
+                denominator = min([ld_obs[1], ld_obs[2]])
                 if denominator == 0:
-                    denominator = max([ld_obs[1],ld_obs[2]])  
+                    denominator = max([ld_obms[1], ld_obs[2]])
             DPrime = D / denominator
-                
+
             # calculate R2
-            R2 = D**2 / ( PA * Pa * PB * Pb )
-            
+            R2 = D**2 / (PA * Pa * PB * Pb)
+
             # cat output
-            print(f"{site_map[first_site]}\t{site_map[second_site]}\t{round(D, 4)}\t{round(DPrime, 4)}\t{round(R2, 4)}")
+            print(
+                f"{site_map[first_site]}\t{site_map[second_site]}\t{round(D, 4)}\t{round(DPrime, 4)}\t{round(R2, 4)}")
 
 
 def main(args):
     logging.info("Reading FASTA data from %s", args.fasta_input)
+    # convert fasta character alignment to integer matrix
     alignment = read_fasta(args.fasta_input)
-    logging.info("Finished reading data. Sequence count: %s, Sequence length %s", *alignment.shape)
-    
-    logging.info("Computing sites of iterest (min_acgt=%s, min_variability=%s)", args.min_acgt, args.min_variability)
-    var_sites = compute_variable_sites(alignment, args.min_acgt, args.min_variability)
-    logging.info("Found %s sites of interest", var_sites.sum())
-    
-    # Trim down the alignment array to only include the sites of interest
-    alignment = alignment[:, var_sites]
-    
-    # Maps site indices in the trimmed down array to site indices in the original alignment
-    site_map = np.where(var_sites)[0]
-    
-    logging.info("Computing Henikoff weights for each sequence")
-    weightsHK = henikoff_weighting(alignment)
+    logging.info(
+        "Finished reading data. Sequence count: %s, Sequence length %s", *alignment.shape)
 
-    weights1 =  np.zeros(alignment.shape[0], dtype=np.uint16)
+    logging.info("Computing sites of iterest (min_acgt=%s, min_variability=%s)",
+                 args.min_acgt, args.min_variability)
+
+    # identify informative sites
+    var_sites_HK, var_sites_LD = compute_variable_sites(
+        alignment, args.min_acgt, args.min_variability)
+    logging.info("Found %s sites of interest", var_sites_LD.sum())
+
+    logging.info("Computing Henikoff weights for each sequence")
+    weightsHK = henikoff_weighting(alignment[:, var_sites_HK])
+
+    # Trim down the alignment array to only include the sites of interest
+    alignment = alignment[:, var_sites_LD]
+
+    # Maps site indices in the trimmed down array to site indices in the original alignment
+    site_map = np.where(var_sites_LD)[0]
+
+    weights1 = np.zeros(alignment.shape[0], dtype=np.uint16)
     weights1[weights1 == 0] = 1
     weights = weights1
-    
+
     logging.info("Computing the LD parameters")
     ld(alignment, weights1, site_map)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="WeightedLD computation tool")
-    parser.add_argument("--fasta-input", type=Path, help="The source file to load", required=True)
+    parser.add_argument("--fasta-input", type=Path,
+                        help="The source file to load", required=True)
     parser.add_argument("--min-acgt", type=float, default=0.8,
-        help="Minimum fractions of ACTG at a given site for the site to be included in calculation.\
+                        help="Minimum fractions of ACTG at a given site for the site to be included in calculation.\
             increase this to remove more noise say 0.5")
     parser.add_argument("--min-variability", type=float, default=0.02,
-        help="Minimum fraction of minor symbols for a site to be considered")
-    
+                        help="Minimum fraction of minor symbols for a site to be considered")
+
     args = parser.parse_args()
     main(args)
---
+++ test.py
@@ -0,0 +1,121 @@
+import unittest
+import io                   # for capturing stdout prints from ld
+import sys
+import WeightedLD as wld    # import the program as module
+import numpy as np
+import re
+
+
+class TestStuff(unittest.TestCase):
+    min_acgt = 0.8
+    min_variability = 0.02
+
+    def test_read_fasta(self):
+        # test the fasta read and hashmap to integers
+        file = "tests/t1_henikoff_paper.fasta"
+        alignment = wld.read_fasta(file)
+
+        self.assertEqual(alignment.sum(), 76)
+
+    def test_var_sitesHK(self):
+        file = "tests/t1_henikoff_paper.fasta"
+        alignment = wld.read_fasta(file)
+        var_sites_HK, var_sites_LD = wld.compute_variable_sites(
+            alignment, self.min_acgt, self.min_variability)
+
+        # non-variable site
+        self.assertEqual(var_sites_HK[0], False)
+        # too many non atgc
+        self.assertEqual(var_sites_HK[1], False)
+        # 0.8 are not acgt and we use > not >=
+        self.assertEqual(var_sites_HK[2], False)
+
+    def test_var_sitesLD(self):
+        # var_stes_ld should have 1 extra False
+        file = "tests/t6_varsites_hk_ld.fasta"
+        alignment = wld.read_fasta(file)
+        min_variability = 0.2
+        var_sites_HK, var_sites_LD = wld.compute_variable_sites(
+            alignment, self.min_acgt, min_variability)
+
+        print(var_sites_HK, var_sites_LD)
+        self.assertNotEqual(var_sites_HK[1], var_sites_LD[1])
+
+    def test_hkw_simple(self):
+        # should return weights as in the 1994 paper, but normal. ignores first two sites as in [test_var_sites]
+        file = "tests/t1_henikoff_paper.fasta"
+        alignment = wld.read_fasta(file)
+        var_sites_HK, var_sites_LD = wld.compute_variable_sites(
+            alignment, self.min_acgt, self.min_variability)
+        alignment = alignment[:, var_sites_HK]
+        weightsHK = wld.henikoff_weighting(alignment)
+
+        self.assertTrue(np.allclose(weightsHK,
+                                    np.array([0.5, 0.5, 0.5, 0.5, 1.0]),
+                                    rtol=1e-02, atol=1e-02))
+
+    def test_hkw_complex(self):
+        # seq0 has a unique base at site0, so is most distinct, ensure this
+        file = "tests/t2_henikoff_complex1.fasta"
+        alignment = wld.read_fasta(file)
+        var_sites_HK, var_sites_LD = wld.compute_variable_sites(
+            alignment, self.min_acgt, self.min_variability)
+        alignment = alignment[:, var_sites_HK]
+        weightsHK = wld.henikoff_weighting(alignment)
+
+        self.assertEqual(weightsHK[0], 1.0)
+
+    def test_hkw_complex_indel(self):
+        # checks indels are handled correctly, here last seq is most unique and contains two insertions
+        file = "tests/t3_henikoff_complex2.fasta"
+        alignment = wld.read_fasta(file)
+        var_sites_HK, var_sites_LD = wld.compute_variable_sites(
+            alignment, self.min_acgt, self.min_variability)
+        alignment = alignment[:, var_sites_HK]
+        weightsHK = wld.henikoff_weighting(alignment)
+
+        self.assertEqual(weightsHK[7], 1.0)
+
+    def test_0ld_flatw_simple(self):
+        # flat weights, no LD, simple example
+        file = "tests/t4_weights1_ld0.fasta"
+        alignment = wld.read_fasta(file)
+        var_sites_HK, var_sites_LD = wld.compute_variable_sites(
+            alignment, self.min_acgt, self.min_variability)
+        weightsHK = wld.henikoff_weighting(alignment[:, var_sites_HK])
+        alignment = alignment[:, var_sites_LD]
+        site_map = np.where(var_sites_LD)[0]
+
+        # capture stdout
+        capturedOutput = io.StringIO()          # Create io object
+        sys.stdout = capturedOutput             # to which we redirect stdout.
+
+        wld.ld(alignment, weightsHK, site_map)  # run function as normal
+
+        sys.stdout = sys.__stdout__             # Reset redirect.
+        self.assertEqual(capturedOutput.getvalue()[22:25], "0.0")     # D
+
+    def test_ld_flatw_simple(self):
+        # flat weights, total LD, simple example
+        file = "tests/t5_weights1_ld0.25.fasta"
+        alignment = wld.read_fasta(file)
+        var_sites_HK, var_sites_LD = wld.compute_variable_sites(
+            alignment, self.min_acgt, self.min_variability)
+        weightsHK = wld.henikoff_weighting(alignment[:, var_sites_HK])
+        alignment = alignment[:, var_sites_LD]
+        site_map = np.where(var_sites_LD)[0]
+
+        # capture stdout
+        capturedOutput = io.StringIO()          # Create io object
+        sys.stdout = capturedOutput             # to which we redirect stdout.
+
+        wld.ld(alignment, weightsHK, site_map)  # run function as normal
+
+        sys.stdout = sys.__stdout__             # Reset redirect.
+
+        self.assertEqual(capturedOutput.getvalue()[22:27], "-0.25")     # D
+        self.assertEqual(capturedOutput.getvalue()[32:33], "1")         # r2
+
+
+if __name__ == '__main__':
+    unittest.main()
